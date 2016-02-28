9855e785c4bd29fa085459f9fbd471d3f0955f2e
lilbake <loko55bass@gmail.com>
2016-02-28 12:34:28 -0500

    test2isbn
    test delete spaces and dashes

--- isbn_program.rb
+++ isbn_program.rb
@@ -1,5 +1,23 @@
 def length_of_isbn(isbn_number)
-	if isbn_number.length != 10 || 13
+	if isbn_number.length == 10
+		true
+	 elsif isbn_number.length == 13
+		true
+	else
 		false
 	end
 end
+
+def remove_dashes_spaces(isbn_number)
+	if isbn_number.include? "-"
+		isbn_number.delete! "-"
+	elsif isbn_number.include? " "
+			isbn_number.delete! " "
+	end
+	isbn_number
+
+end
+
+
+
+
--- isbn_test.rb
+++ isbn_test.rb
@@ -7,5 +7,17 @@ class ISBNVerify <Minitest::Test
 	def test_isbn_string_length_other_than_10_or_13_false
 		assert_equal(false, length_of_isbn("97804700590291"))
 		assert_equal(false, length_of_isbn("04719586971"))
+		assert_equal(false, length_of_isbn(""))
 	end
+
+	def test_isbn_string_length_other_than_10_or_13_true
+		assert_equal(true, length_of_isbn("9780470059029"))
+		assert_equal(true, length_of_isbn("0471958697"))
+	end
+
+	def test_for_no_dashes_or_spaces
+		assert_equal("9780470059029", remove_dashes_spaces("978-047-005-9029"))
+		assert_equal("9780470059029", remove_dashes_spaces("978 047 005 9029"))		
+	end
+
 end
