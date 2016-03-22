0c912f4e27fb86f9bc543d2ce78b48d1557c1cc5
lilbake <loko55bass@gmail.com>
2016-03-22 19:12:55 -0400

    isbn program
    completed isbn program 100% functional test and working csv files linked
    to isbn program. all validity corrected.

--- class_isbn_kata.rb
+++ class_isbn_kata.rb
@@ -1,27 +1,23 @@
 def valid_isbn?(input_string)
     string_with_no_spaces_and_hyphens = remove_invalid_characters(input_string)
-    valid_length = correct_length?(string_with_no_spaces_and_hyphens)
-    non_numerical = contains_non_numerical_characters?(string_with_no_spaces_and_hyphens)
-    non_numerical == true && valid_length == true
+    correct_length?(string_with_no_spaces_and_hyphens)
 end
-
 
 def remove_invalid_characters(input_string)
     no_spaces = input_string.delete(" ")
     no_hyphens = no_spaces.delete("-")
 end
 
-
 def correct_length?(input_string)
-    if input_string.length == 10     
-        true
-    elsif input_string.length == 13 && isbn13_math?(input_string)
-        true
+    if input_string.length == 10 && contains_non_numerical_characters?(input_string[0...8]) == false
+        isbn_10_math?(input_string)
+    elsif input_string.length == 13 && contains_non_numerical_characters?(input_string) == false
+        isbn13_math?(input_string)
+
     else    
         false
     end
 end    
-
 
 def isbn_10(string)
     sum = 0
@@ -34,13 +30,11 @@ def isbn_10(string)
     sum
 end
 
-
 def isbn_10_mod_11(number)
     checksum = number%11
 end
 
-
-def isbn_10_containing_x?(number)
+def isbn_10_math?(number)
     sum = isbn_10(number)
     checksum = isbn_10_mod_11(sum)
     
@@ -98,8 +92,8 @@ def subtraction(number)
     final_result = mod_result%10
 end
 
-def contains_non_numerical_characters?(input_string)
-    if input_string =~/\D/
+def contains_non_numerical_characters?(string)
+    if string =~/\D/
         true
     else 
         false
--- class_isbn_kata_test.rb
+++ class_isbn_kata_test.rb
@@ -1,12 +1,8 @@
-	
-
- 
 require 'minitest/autorun'
+
 require_relative 'class_isbn_kata.rb'
 
-
 class TestIsbnChecker < Minitest::Test
-
 
     def test_string_without_10_or_13_numbers
         assert_equal(false, valid_isbn?(""))
@@ -14,31 +10,25 @@ class TestIsbnChecker < Minitest::Test
         assert_equal(false, valid_isbn?("12345678912345"))
     end
 
-
     def test_valid_isbn10_returns_true
         assert_equal(true, valid_isbn?("0471958697"))
     end
-
 
     def test_valid_isbn10_with_spaces_returns_true
         assert_equal(true, valid_isbn?("047 1958 697"))
     end
 
-
     def test_valid_isbn10_with_hyphens_returns_true
         assert_equal(true, valid_isbn?("0-321-14653-0"))
     end
-
 
     def test_valid_isbn10_with_spaces_and_hyphens_returns_true
         assert_equal(true, valid_isbn?(" 047-1958 697-"))
     end
 
-
     def test_valid_isbn13_returns_true
         assert_equal(true, valid_isbn?("9780470059029"))
     end
-
 
     def test_valid_isbn13_with_spaces_and_hyphens_returns_true
         assert_equal(true, valid_isbn?("-9  7---80470059029"))
@@ -46,68 +36,63 @@ class TestIsbnChecker < Minitest::Test
         assert_equal(true, valid_isbn?("978 0 471 48648 0"))
     end
 
-
     def test_invalid_isbn_13_returns_false
         assert_equal(false, valid_isbn?("4780470059029"))
     end    
 
-
     def test_convert_string_to_array_characters
         assert_equal(["1","2","3","4","5","6","7"], convert_string_to_array("1234567"))
     end    
-
 
     def test_multiply_array_by_1_and_3
         assert_equal([1,3,1,3,1,3], multiply_array(["1","1","1","1","1","1"]))
         assert_equal([2,9,4,15,6], multiply_array(["2","3","4","5","6"]))
     end    
 
-
     def test_sum_all_but_last_items_in_array
         assert_equal(6, sum_of_items([1,2,3,4]))
         assert_equal(18, sum_of_items([5,6,7,8]))
     end    
 
-
     def test_module_10_subtraction_result
         assert_equal(4, subtraction(26))
         assert_equal(0, subtraction(10))
-    end    
-        def test_isbn_13_checksum
+    end
+
+    def test_isbn_13_checksum
         assert_equal(true, isbn13_math?("9780470059029"))
         assert_equal(true, isbn13_math?("9780131495050"))
     end    
-
 
     def test_isbn_10_multiply_sum
         assert_equal(232, isbn_10("7421394761"))
         assert_equal(254, isbn_10("5478931865"))
     end                              
 
-
     def test_isbn_10_mod_11
         assert_equal(1, isbn_10_mod_11(232))
         assert_equal(2, isbn_10_mod_11(376))
     end    
 
-
     def test_isbn_10_containing_x
-        assert_equal(true, isbn_10_containing_x?("877195869x"))
-        assert_equal(false, isbn_10_containing_x?("877195869@"))
+        assert_equal(true, isbn_10_math?("877195869x"))
+        assert_equal(false, isbn_10_math?("877195869@"))
     end    
 
-
-    def test_valid_or_invalid_isbn
+    def test_invalid_isbn
         assert_equal(false, valid_isbn?("4780470059029"))
         assert_equal(false, valid_isbn?("0-321@14653-0"))
         assert_equal(false, valid_isbn?("877195x869"))
         assert_equal(false, valid_isbn?("978-O-13-149505-0"))
+        assert_equal(false, valid_isbn?("0471258697"))
+    end
+
+    def test_valid_isbn
         assert_equal(true, valid_isbn?("0471958697"))
-        assert_equal(true, valid_isbn?("-321-14653-0"))
+        assert_equal(true, valid_isbn?("0-321-14653-0"))
         assert_equal(true, valid_isbn?("877195869x"))
         assert_equal(true, valid_isbn?("9780470059029"))
         assert_equal(true, valid_isbn?("978-0-13-149505-0"))
         assert_equal(true, valid_isbn?("978 0 471 48648 0"))
     end
-
 end
