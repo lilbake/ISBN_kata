6e3bd49497e579dba3da454979f057c94d812dc8
lilbake <loko55bass@gmail.com>
2016-04-29 17:26:33 -0400

    tic tac toe ai game
    making an ai playable tictactoe game

--- ttt_ai_game.rb
+++ ttt_ai_game.rb
@@ -1,13 +1,13 @@
 require_relative "tictactoe_sai.rb"
 require_relative "tictactoe_seq_sai.rb"
 
-def play_game (player_1, player_2, board)
+def play_game (current_player, opponent, board)
 
-	move = player_1[:player_mode].get_move(board)
-	board = board_update(board, move -1, player_1[:marker])
-	if game_over?(board, player_1[:marker])
+	move = current_player[:player_mode].get_move(board)
+	board = board_update(board, move -1, current_player[:marker])
+	if game_over?(board, current_player[:marker])
 		board
 	else
-		play_game(player_2, player_1, board)
+		play_game(opponent, current_player, board)
 	end
 end
